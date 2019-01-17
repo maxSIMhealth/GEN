63267e9ab70c8c86a4af0752cfdfb07a3bcb1878
Andrei Bosco Bezerra Torres <andrei.torres@uoit.net>
2019-01-16 21:54:31 -0500

    improving overall templates and starting comments template

--- OPEN2/OPEN2/urls.py
+++ OPEN2/OPEN2/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from forums import views
 
 urlpatterns = [    
-    path('', views.home, name="home"),
+    path('', views.home, name='home'),
+    path('forums/<pk>/', views.forum_comments, name='forum_comments'),
     path('admin/', admin.site.urls),
 ]
--- OPEN2/forums/views.py
+++ OPEN2/forums/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Forum
 
 def home(request):
   forums = Forum.objects.all()
-
   return render(request, 'home.html', {'forums': forums})
+
+def forum_comments(request, pk):
+  forum = get_object_or_404(Forum, pk=pk)
+  return render(request, 'comments.html', {'forum': forum})
