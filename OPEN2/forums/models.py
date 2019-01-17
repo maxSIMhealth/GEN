6c2ae8a98b4f7b35fac091ce325d84e7f39e394f
Andrei Bosco Bezerra Torres <andrei.torres@uoit.net>
2019-01-16 23:06:18 -0500

    adding author to Forum model

--- OPEN2/forums/models.py
+++ OPEN2/forums/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Forum(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forums') # TODO: implement author
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='forums')
 
     def __str__(self):
         return self.name
