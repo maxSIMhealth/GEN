2571a5cab7ef96976dbdcb440ede6956bc4f88e4
Andrei Bosco Bezerra Torres <andrei.torres@uoit.net>
2019-01-18 11:13:34 -0500

    creating 'accounts' app

---
+++ OPEN2/accounts/views.py
@@ -0,0 +1,15 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
+
+def signup(request):
+  if request.method == 'POST':
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+  else:
+    form = SignUpForm()
+  return render(request, 'signup.html', {'form': form})
