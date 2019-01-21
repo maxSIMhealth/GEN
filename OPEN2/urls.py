ef818219d215de381f965ddb3ba46b70ad182706
Andrei Bosco Bezerra Torres <andrei.torres@uoit.net>
2019-01-21 11:03:28 -0500

    adding 'user account' view

--- OPEN2/urls.py
+++ OPEN2/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path("settings/password", auth_views.PasswordChangeView.as_view(template_name='password_change.html'), name="password_change"),
     path("settings/password/done", auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name="password_change_done"),
+    path("settings/account", account_views.UserUpdateView.as_view(), name='my_account'),
     path('forums/', views.ForumListView.as_view(), name='home'),
     path('forums/new/', views.new_forum, name='new_forum'),
     path('forums/<int:pk>/', views.forum_comments, name='forum_comments'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import UpdateView
 
 from .forms import SignUpForm
 
@@ -13,3 +18,13 @@ def signup(request):
   else:
     form = SignUpForm()
   return render(request, 'signup.html', {'form': form})
+
+@method_decorator(login_required, name='dispatch')
+class UserUpdateView(UpdateView):
+  model = User
+  fields = ('first_name', 'last_name', 'email', )
+  template_name = 'my_account.html'
+  success_url = reverse_lazy('my_account')
+
+  def get_object(self):
+    return self.request.user
