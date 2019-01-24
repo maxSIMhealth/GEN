6f6d26b835a7decdfa0b7f3bb342415ea1733e02
Andrei Bosco Bezerra Torres <andrei.torres@uoit.net>
2019-01-24 11:49:48 -0500

    adding course description field

--- forums/models.py
+++ forums/models.py
@@ -5,10 +5,11 @@ from vote.models import VoteModel
 
 class Course(models.Model):
     name = models.CharField(max_length=30, unique=True)
+    code = models.CharField('course code', max_length=30)
+    description = models.CharField(max_length=100)
     author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='course')
     start_date = models.DateTimeField('start date', blank=True, null=True)
     end_date = models.DateTimeField('end date', blank=True, null=True)
-    code = models.CharField('course code', max_length=30)
     students = models.ManyToManyField(User, related_name='member')
 
     def __str__(self):
