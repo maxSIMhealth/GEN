59231c6747908386d29021ee39e61501f11dbe95
Andrei Bosco Bezerra Torres <andrei.torres@uoit.net>
2019-01-26 14:42:31 -0500

    changing quiz into TimeStampModel (adds 'created' and 'modified' fields)

--- quiz/models.py
+++ quiz/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from model_utils.models import TimeStampedModel
 
 from forums.models import Course, MediaFile
 
-class Quiz(models.Model):
-  name = models.CharField(max_length=30, unique=True)
-  description = models.CharField(max_length=100)
-  author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='quizzes')
-  course = models.ForeignKey(Course, on_delete=models.PROTECT, related_name='quizzes')
-  video = models.ForeignKey(MediaFile, on_delete=models.PROTECT, related_name='quizzes')
+class Quiz(TimeStampedModel):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='quizzes')
+    course = models.ForeignKey(Course, on_delete=models.PROTECT, related_name='quizzes')
+    video = models.ForeignKey(MediaFile, on_delete=models.PROTECT, related_name='quizzes')
 
-  class Meta:
-    verbose_name_plural = "quizzes"
+    class Meta:
+        verbose_name_plural = "quizzes"
 
-  def __str__(self):
-      return self.name
+    def __str__(self):
+        return self.name
