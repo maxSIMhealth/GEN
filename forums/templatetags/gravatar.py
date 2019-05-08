425203e3f8fe97958530e66c3489835933f624f9
Andrei Bosco Bezerra Torres <andrei.torres@uoit.net>
2019-05-08 17:25:13 +0000

    changing default gravatar image to robohash

--- forums/templatetags/gravatar.py
+++ forums/templatetags/gravatar.py
@@ -9,7 +9,7 @@ register = template.Library()
 @register.filter
 def gravatar(user):
     email = user.email.lower().encode('utf-8')
-    default = 'mm'
+    default = 'robohash'
     size = 256
     url = 'https://www.gravatar.com/avatar/{md5}?{params}'.format(
         md5=hashlib.md5(email).hexdigest(),
