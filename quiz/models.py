1ea87c7dc105d0892f8c52cb4fbcf38622cc1826
Andrei Bosco Bezerra Torres <607297+andreibosco@users.noreply.github.com>
2020-03-23 09:53:30 -0400

    Working on #34
    - Quiz now shows user uploaded video

--- quiz/models.py
+++ quiz/models.py
@@ -3,15 +3,18 @@ from django.contrib.auth.models import User
 from model_utils.models import TimeStampedModel
 from model_utils.managers import InheritanceManager
 
-from forums.models import Course, MediaFile
+from forums.models import Course, VideoFile
 
 
 class Quiz(TimeStampedModel):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-    author = models.ForeignKey(User, on_delete=models.PROTECT, related_name='quizzes')
-    course = models.ForeignKey(Course, on_delete=models.PROTECT, related_name='quizzes')
-    video = models.ForeignKey(MediaFile, on_delete=models.PROTECT, related_name='quizzes')
+    author = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name='quizzes')
+    course = models.ForeignKey(
+        Course, on_delete=models.PROTECT, related_name='quizzes')
+    video = models.ForeignKey(
+        VideoFile, on_delete=models.PROTECT, related_name='quizzes')
 
     class Meta:
         verbose_name_plural = "quizzes"
