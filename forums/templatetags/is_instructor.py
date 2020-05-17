8ccf0e1b9b9d1bb69346c7ae6403bcd47ae80817
Andrei Torres <andreibosco@gmail.com>
2020-05-17 14:10:23 -0400

    Fixes #109
    - Replaced usage of templatetag 'has_group' for just checking if the user is marked as 'is_staff' (did not delete 'has_group' because it can still be useful in the future)

--- forums/templatetags/is_instructor.py
+++ forums/templatetags/is_instructor.py
@@ -1,9 +1,8 @@
 from django import template
-from django.contrib.auth.models import Group
 
 register = template.Library()
 
 
-@register.filter(name='is_instructor')
+@register.filter(name="is_instructor")
 def is_instructor(user, course):
     return bool(course in user.instructor.all())
