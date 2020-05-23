ca7303f40446e4c56eb0c0a14e2765acebcc15a4
Andrei Bosco Bezerra Torres <andreibosco@gmail.com>
2020-05-23 15:27:31 -0400

    Allow instructor or staff to access any section - Improving code from the previous commit

--- courses/support_methods.py
+++ courses/support_methods.py
@@ -10,20 +10,24 @@ def requirement_fulfilled(user, section):
 
     fulfilled = False
     items_completed = []
+    # check if user is a course instructor or system staff
+    is_instructor = bool(section.course in user.instructor.all())
+    if is_instructor or user.is_staff:
+        fulfilled = True
+    else:
+        for item in requirement.section_items.all():
+            if requirement.section_type == "Q":
+                items_completed.append(quiz_score_get(user, item.quiz).exists())
+            elif requirement.section_type == "D":
+                items_completed.append(has_participated(user, item.discussion))
+            elif requirement.section_type == "U":
+                if requirement.section_items.count() > 0:
+                    items_completed.append(True)
+            elif requirement.section_type == "V":
+                if item.videofile.quizzes.exists():
+                    for quiz in item.videofile.quizzes.all():
+                        items_completed.append(quiz_score_get(user, quiz).exists())
 
-    for item in requirement.section_items.all():
-        if requirement.section_type == "Q":
-            items_completed.append(quiz_score_get(user, item.quiz).exists())
-        elif requirement.section_type == "D":
-            items_completed.append(has_participated(user, item.discussion))
-        elif requirement.section_type == "U":
-            if requirement.section_items.count() > 0:
-                items_completed.append(True)
-        elif requirement.section_type == "V":
-            if item.videofile.quizzes.exists():
-                for quiz in item.videofile.quizzes.all():
-                    items_completed.append(quiz_score_get(user, quiz).exists())
-
-        fulfilled = all(element for element in items_completed)
+            fulfilled = all(element for element in items_completed)
 
     return fulfilled
