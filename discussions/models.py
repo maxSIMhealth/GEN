c27348c819fcfa7588dcee18a597459441778427
Andrei Bosco Bezerra Torres <andreibosco@fastmail.com>
2020-06-15 13:01:21 -0400

    Translation: model 'discussions'

--- discussions/models.py
+++ discussions/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from vote.models import VoteModel
 
 from courses.models import Course, SectionItem
@@ -14,14 +15,21 @@ def user_directory_path(instance, filename):
 class Discussion(VoteModel, SectionItem):
     related_name = "discussions"
     course = models.ForeignKey(
-        Course, on_delete=models.PROTECT, related_name=related_name
+        Course,
+        on_delete=models.PROTECT,
+        related_name=related_name,
+        verbose_name=_("course"),
     )
-    last_updated = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(_("last updated"), auto_now_add=True)
     # FIXME: media should be renamed to document or something like that
     # media = models.ForeignKey(
     #     MediaFile, on_delete=models.CASCADE, related_name='discussions')
     requirement = models.ForeignKey(
-        "self", on_delete=models.PROTECT, blank=True, null=True,
+        "self",
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True,
+        verbose_name=_("requirement"),
     )
     video = models.ForeignKey(
         VideoFile,
@@ -29,19 +37,36 @@ class Discussion(VoteModel, SectionItem):
         related_name=related_name,
         blank=True,
         null=True,
+        verbose_name=_("video"),
     )
+
+    class Meta:
+        verbose_name = _("discussion")
+        verbose_name_plural = _("discussions")
 
     def get_comment_count(self):
         return Discussion.objects.filter(comments__discussion=self).count()
 
 
 class Comment(VoteModel, models.Model):
-    message = models.TextField(max_length=400)
+    message = models.TextField(_("message"), max_length=400)
     discussion = models.ForeignKey(
-        Discussion, on_delete=models.CASCADE, related_name="comments"
+        Discussion,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name=_("discussion"),
     )
-    created_at = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateTimeField(_("created at"), auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments",
+        verbose_name=_("author"),
+    )
+
+    class Meta:
+        verbose_name = _("comment")
+        verbose_name_plural = _("comments")
 
     def __str__(self):
         return self.message
