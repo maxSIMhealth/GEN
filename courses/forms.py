a2e1cc48aecb9e64efad6f4dbdce8cc5a78b67bd
Andrei Bosco Bezerra Torres <andreibosco@fastmail.com>
2020-06-22 11:05:12 -0400

    Improve courses admin form
    - Improved filtering of section admin page

--- courses/forms.py
+++ courses/forms.py
@@ -8,10 +8,16 @@ class SectionAdminForm(forms.ModelForm):
         super(SectionAdminForm, self).__init__(*args, **kwargs)
         # check if the instance exists (if its a new object or not)
         if self.instance.pk is not None:
-            # lists only sections that are related to the course
-            self.fields["requirement"].queryset = Section.objects.filter(
-                course=self.instance.course
-            )
-            self.fields["section_output"].queryset = Section.objects.filter(
-                course=self.instance.course
-            )
+            # lists only sections that are related to the course and
+            # exclude itself from requirement and output
+            self.fields["requirement"].queryset = Section.objects.exclude(
+                pk=self.instance.pk
+            ).filter(course=self.instance.course)
+            # only upload sections can have an output, which must be a discussion section
+            if self.instance.section_type == "U":
+                self.fields["section_output"].queryset = Section.objects.exclude(
+                    pk=self.instance.pk
+                ).filter(course=self.instance.course, section_type="D")
+            else:
+                # return an empty queryset
+                self.fields["section_output"].queryset = Section.objects.none()
