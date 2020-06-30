f9662dc5ef729087905be242d84f821e625cc5dc
Andrei Bosco Bezerra Torres <andreibosco@fastmail.com>
2020-06-30 17:14:11 -0400

    Closes #92

--- GEN/urls.py
+++ GEN/urls.py
@@ -47,6 +47,26 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     # path("settings/", account_views.UserUpdateView.as_view(), name='my_account'),
     path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+    ),
+    url(
+        r"^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$",
+        auth_views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+    ),
+    path(
         "settings/password",
         auth_views.PasswordChangeView.as_view(template_name="password_change.html"),
         name="password_change",
