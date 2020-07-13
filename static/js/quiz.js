2866a6be34adc91265111fd7a634bfde5110ebcc
Andrei Bosco Bezerra Torres <andreibosco@fastmail.com>
2020-07-13 11:43:11 -0400

    Continuing #171
    - Renamed all 'slides' references to 'pagination'

--- static/js/quiz.js
+++ static/js/quiz.js
@@ -1,98 +1,96 @@
-var slideInitialValue = 1;
-var slideIndex = slideInitialValue;
-var slides = document.getElementsByClassName("quiz-item");
-var dots = document.getElementsByClassName("dot");
+var paginationInitialValue = 1;
+var paginationIndex = paginationInitialValue;
+var pagination = document.getElementsByClassName("quiz-item");
+// var dots = document.getElementsByClassName("dot");
 
-showSlides(slideIndex);
+showPagination(paginationIndex);
 
 function updateNavigationText() {
-  document.getElementById("quiz-navigation-position").innerHTML = slideIndex + " of " + slides.length;
+  document.getElementById("quiz-navigation-position").innerHTML = paginationIndex + " of " + pagination.length;
 }
 
-function previousSlide() {
-  if (slideIndex > slideInitialValue) {
-    slideIndex--;
-    showSlides(slideIndex);
+function previousPaginationItem() {
+  if (paginationIndex > paginationInitialValue) {
+    paginationIndex--;
+    showPagination(paginationIndex);
   } else {
-    console.log(slideIndex + " is the beginning of the slide: " + slideInitialValue + " of " + slides.length);
+    console.log(paginationIndex + " is the beginning of the pagination: " + paginationInitialValue + " of " + pagination.length);
   }
 }
 
-function nextSlide() {
-  if (slideIndex < slides.length) {
-    slideIndex++;
-    showSlides(slideIndex);
+function nextPaginationItem() {
+  if (paginationIndex < pagination.length) {
+    paginationIndex++;
+    showPagination(paginationIndex);
   } else {
-    console.log(slideIndex + " is the end of the slide length: " + slideInitialValue + " of " + slides.length);
+    console.log(paginationIndex + " is the end of the pagination length: " + paginationInitialValue + " of " + pagination.length);
   }
 }
 
-function slideNavLinkEnable(object) {
+function paginationNavLinkEnable(object) {
   var element = document.querySelector(object);
   element.classList.remove("disabled");
   element.querySelector("a").removeAttribute("tabindex");
   element.querySelector("a").removeAttribute("aria-disabled");
 }
 
-function slideNavLinkDisable(object) {
+function paginationNavLinkDisable(object) {
   var element = document.querySelector(object);
   element.classList.add("disabled");
   element.querySelector("a").setAttribute("tabindex","-1");
   element.querySelector("a").setAttribute("aria-disabled","true");
 }
 
-function goToSlide(slideNumber) {
-  var slideRangeCheck = checkSlideRange(slideNumber);
-  if (slideRangeCheck) {
-    slideIndex = slideNumber;
-    showSlides(slideNumber);
+function goToPaginationItem(paginationItemNumber) {
+  var paginationRangeCheck = checkPaginationRange(paginationItemNumber);
+  if (paginationRangeCheck) {
+    paginationIndex = paginationItemNumber;
+    showPagination(paginationItemNumber);
   } else {
-    console.warn("Slide number out of range");
+    console.warn("Pagination item number out of range");
   }
 }
 
-function checkSlideRange(slideNumber) {
-  if (slideNumber > slides.length || slideNumber < slideInitialValue) {
+function checkPaginationRange(paginationItemNumber) {
+  if (paginationItemNumber > pagination.length || paginationItemNumber < paginationInitialValue) {
     return false;
   } else {
     return true;
   }
 }
 
-function showSlides(slideNumber) {
+function showPagination(paginationItemNumber) {
   var i;
 
-  // console.log("current slide " + slideNumber);
+  var paginationRangeOk = checkPaginationRange(paginationItemNumber);
 
-  var slideRangeCheck = checkSlideRange(slideNumber);
-
-  if (slideRangeCheck) {
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+  if (paginationRangeOk) {
+    for (i = 0; i < pagination.length; i++) {
+      pagination[i].style.display = "none";
     }
 
     // for (i = 0; i < dots.length; i++) {
     //   dots[i].className = dots[i].className.replace(" active", "");
     // }
 
-    slides[slideNumber-1].style.display = "block";
-    // dots[slideIndex-1].className += " active";
+    pagination[paginationItemNumber-1].style.display = "block";
+    // dots[paginationIndex-1].className += " active";
 
     updateNavigationText();
 
-    if (slideNumber > slideInitialValue) {
-      slideNavLinkEnable("#quiz-navigation-previous");
-    } else if (slideNumber == slideInitialValue) {
-      slideNavLinkDisable("#quiz-navigation-previous");
+    if (paginationItemNumber > paginationInitialValue) {
+      paginationNavLinkEnable("#quiz-navigation-previous");
+    } else if (paginationItemNumber == paginationInitialValue) {
+      paginationNavLinkDisable("#quiz-navigation-previous");
     }
 
-    if (slideNumber == slides.length) {
-      slideNavLinkDisable("#quiz-navigation-next");
-    } else if (slideNumber < slides.length) {
-      slideNavLinkEnable("#quiz-navigation-next");
+    if (paginationItemNumber == pagination.length) {
+      paginationNavLinkDisable("#quiz-navigation-next");
+    } else if (paginationItemNumber < pagination.length) {
+      paginationNavLinkEnable("#quiz-navigation-next");
     }
   } else {
-    console.warn("Slide number out of range");
+    console.warn("Pagination item number out of range");
   }
 }
 
