2266738fa662d17925bb4379ed3184847a30555e
Andrei Torres <andreibosco@fastmail.com>
2021-05-12 16:39:02 -0400

    Cleaning up show_sections.py

--- app/courses/templatetags/show_sections.py
+++ app/courses/templatetags/show_sections.py
@@ -1,17 +1,17 @@
 from django import template
 
-from courses.support_methods import requirement_fulfilled
 from core.views import course_sections_list
+from courses.support_methods import requirement_fulfilled
 
 register = template.Library()
 
 
 @register.inclusion_tag("section_tabs.html", takes_context=True)
-def show_sections(context, sections):
+def show_sections(context):
     user = context["user"]
     course = context["course"]
     sections = course_sections_list(course, user)
-    # sections = sections.all()
+
     if "section" in context:
         current_section = context["section"]
     else:
