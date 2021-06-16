fac74d928c2a4028ae593525cab263278638d69a
Andrei Torres <andreibosco@fastmail.com>
2021-06-15 20:09:09 -0400

    Fixed message tags - Adjusted message tags values to fit expected format of new 'bootstrap_alert' templatetag

--- app/GEN/settings.py
+++ app/GEN/settings.py
@@ -134,11 +134,11 @@ TEMPLATES = [
 ]
 
 MESSAGE_TAGS = {
-    messages.DEBUG: "alert-info",
-    messages.INFO: "alert-info",
-    messages.SUCCESS: "alert-success",
-    messages.WARNING: "alert-warning",
-    messages.ERROR: "alert-danger",
+    messages.DEBUG: "secondary",
+    messages.INFO: "info",
+    messages.SUCCESS: "success",
+    messages.WARNING: "warning",
+    messages.ERROR: "danger",
 }
 
 AUTHENTICATION_BACKENDS = (
