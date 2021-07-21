5798b4cf744bfb932cc9582728387fc676511a11
Andrei Torres <andreibosco@fastmail.com>
2021-07-21 12:59:43 -0400

    Closes #274
    Filtering to only use items set as published

--- app/courses/progress.py
+++ app/courses/progress.py
@@ -1,4 +1,5 @@
 def progress(user, items):
+    items = items.filter(published = True)
     items_total = items.__len__()
     items_participation = 0
 
@@ -10,7 +11,7 @@ def progress(user, items):
             if item.quizscore_set.filter(student=user).exists():
                 items_participation += 1
         if item._meta.model_name == "section":
-            if item.status.filter(learner=user,completed=True).exists():
+            if item.status.filter(learner=user, completed=True).exists():
                 items_participation += 1
 
     items_progress = {"max": items_total, "current": items_participation}
