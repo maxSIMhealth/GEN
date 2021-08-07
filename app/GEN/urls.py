d73dbdb459687dd1f2619c1ab374266542ca7ab4
Andrei Torres <andreibosco@fastmail.com>
2021-08-07 13:40:15 -0400

    Possible fix for #290

--- app/GEN/urls.py
+++ app/GEN/urls.py
@@ -38,9 +38,12 @@ urlpatterns = [
         "robots.txt",
         TemplateView.as_view(template_name="robots.txt", content_type="text/plain"),
     ),
-    path(
-        "favicon.ico",
-        RedirectView.as_view(url=staticfiles_storage.url("img/favicon.ico")),
+    re_path(
+        r"^favicon.ico$",
+        RedirectView.as_view(
+            url=staticfiles_storage.url("favicon.ico"),
+            permanent=False),
+        name="favicon"
     ),
 ]
 
