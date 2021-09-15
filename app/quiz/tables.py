eda4bf209b620421d3b3ac259f1eb97bbc7ce1f0
Andrei Torres <andreibosco@fastmail.com>
2021-09-15 12:27:54 -0400

    Ordering quiz results table by item id

--- app/quiz/tables.py
+++ app/quiz/tables.py
@@ -1,8 +1,10 @@
 import django_tables2 as tables
 from .models import QuestionAttempt
 
+
 class QuestionAttemptTable(tables.Table):
     class Meta:
         model = QuestionAttempt
         template_name = "django_tables2/bootstrap4.html"
         fields = ("id", "question", "multiplechoice_answer", "answer_content")
+        order_by = "id"
