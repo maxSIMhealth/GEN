0ca50e7f99a6e4e3a9dabbd393f7089429ec2191
Andrei Torres <andreibosco@fastmail.com>
2021-09-30 12:59:03 -0400

    Removed participant information from quiz result list data

--- app/quiz/tables.py
+++ app/quiz/tables.py
@@ -6,7 +6,7 @@ class QuestionAttemptTable(tables.Table):
     class Meta:
         model = QuestionAttempt
         template_name = "django_tables2/bootstrap4.html"
-        fields = ("id", "student", "question", "multiplechoice_answer", "answer_content")
+        fields = ("id", "question", "multiplechoice_answer", "answer_content")
         order_by = "id"
 
     def before_render(self, request):
