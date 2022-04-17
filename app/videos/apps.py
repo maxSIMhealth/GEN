70d076a604a9831bd985e0005f803666a16f50d7
Andrei Torres <andreibosco@fastmail.com>
2022-04-16 22:18:24 -0400

    Fixed #367

--- app/videos/apps.py
+++ app/videos/apps.py
@@ -1,11 +1,8 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_save
 
 
 class VideosConfig(AppConfig):
     name = "videos"
 
     def ready(self):
-        from videos import signals
-
-        post_save.connect(signals.generate_thumbnail)
+        from . import signals  # noqa: F401
