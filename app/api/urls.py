131234819833fa80d039ff64513f83e07d7b8413
Andrei Bosco Bezerra Torres <andrei.torres@ontariotechu.net>
2022-05-10 12:16:51 -0400

    Closed #395 #396 #397 #398 #406 #399 #400 #401 #407

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,11 +2,37 @@ from rest_framework import routers
 
 from django.urls import include, path
 
+from .views import (
+    CourseDetailApiView,
+    CourseListApiView,
+    SectionDetailApiView,
+    SectionItemDetailApiView,
+    UserRecordView,
+)
+
 app_name = "api"
 
 # Registering API routes
 router = routers.DefaultRouter(trailing_slash=False)
+# router.register('courses', CourseApiViewSet)
+# router.register('sections', SectionApiViewSet)
+# router.register('sections_test', SectionViewSet)
+# router.register('user', UserRecordView.as_view(), basename="user")
 
 urlpatterns = [
+    # path("api/user/", UserRecordView.as_view(), name="user-record"),
     path("", include(router.urls)),
+    path("user/", UserRecordView.as_view(), name="users"),
+    path("courses/", CourseListApiView.as_view(), name="course-list"),
+    path("courses/<int:pk>/", CourseDetailApiView.as_view(), name="course-detail"),
+    path(
+        "courses/<int:course_pk>/section/<int:pk>/",
+        SectionDetailApiView.as_view(),
+        name="section-detail",
+    ),
+    path(
+        "courses/<int:course_pk>/section/<int:section_pk>/item/<int:pk>/",
+        SectionItemDetailApiView.as_view(),
+        name="section-item-detail",
+    ),
 ]
