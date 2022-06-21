518b13f54c5eabff7431b958842bfc82bb10d2fa
Andrei Bosco Bezerra Torres <andrei.torres@ontariotechu.net>
2022-06-21 17:19:49 -0400

    Re-enabled session and token authentication.

--- app/GEN/settings.py
+++ app/GEN/settings.py
@@ -521,11 +521,11 @@ if USE_EMAIL_DOMAINS_WHITELIST:
 # REST Framework
 #
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.SessionAuthentication',
-    #     'rest_framework.authentication.TokenAuthentication',
-    # ],
-    # 'DEFAULT_PERMISSION_CLASSES': [
-    #     'rest_framework.permissions.IsAuthenticated',
-    # ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
