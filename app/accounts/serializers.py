b20915b6f6c64d70efeaacfaf6c071e85e7b279a
Andrei Bosco Bezerra Torres <andrei.torres@ontariotechu.net>
2022-06-21 17:20:02 -0400

    Accounts: disabled password field.

--- app/accounts/serializers.py
+++ app/accounts/serializers.py
@@ -16,7 +16,7 @@ class UserSerializer(serializers.ModelSerializer):
             "first_name",
             "last_name",
             "email",
-            "password",
+            # "password",
         )
         validators = [
             UniqueTogetherValidator(
