11398ce230591c5b125395912673352715d4dc6b
Andrei Bosco Bezerra Torres <andrei.torres@ontariotechu.net>
2023-02-01 13:44:00 -0500

    Fixed #479
    - Added warning message.
    - Added tooltip message.
    - Removed old message that was shown only for paginated quizzes.

--- app/quiz/views.py
+++ app/quiz/views.py
@@ -345,6 +345,12 @@ def quiz_page(request, pk, section_pk, sectionitem_pk):
                     questions_ids.append(question.pk)
                 request.session["quiz_questions"] = questions_ids
 
+                # if the quiz author is accessing, warn that the quiz can not be submitted
+                messages.warning(
+                    request,
+                    "This quiz is in PREVIEW mode. Submitting is DISABLED because you are the author.",
+                )
+
                 # if the max number of attempts has been reached, redirect back to quiz list
                 if attempts_left <= 0:
                     messages.error(
