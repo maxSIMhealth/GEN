04cf67c5ab7c86e1c418e9582cfd8a20694983d1
Andrei Bosco Bezerra Torres <andrei.torres@ontariotechu.net>
2023-04-18 12:16:04 -0400

    Minor refactoring on dashboard view and template
    - Course progress now is added into the course object as a parameter, removing the need to use a templatetag in the template

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1,4 +1,4 @@
-from courses.support_methods import requirement_fulfilled
+from courses.support_methods import progress, requirement_fulfilled
 
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
@@ -17,18 +17,23 @@ def check_items_requirement(user, items):
 def dashboard(request):
     user = request.user
 
-    # get courses that the user is a member of, and sort them by group
-    courses_all = user.member.all().order_by("group", "custom_order")
-
     # get dashboard information, if any exists and is set as active
     try:
         dashboard_info = DashboardSetting.objects.filter(active=True)[0]
     except IndexError:
         dashboard_info = None
 
+    # get courses that the user is a member of, and sort them by group
+    courses_all = user.member.all().order_by("group", "custom_order")
+
+    # add course progress information to course objects
+    get_course_progress(user, courses_all)
+
     # check requirements
     check_items_requirement(user, courses_all)
-    # check_items_requirement(user, modules)
+
+    # check if course groups have all requirements fulfilled
+    # groups_objects = Group.objects.filter(user=user)
 
     return render(
         request,
@@ -39,3 +44,16 @@ def dashboard(request):
             # "modules": modules
         },
     )
+
+
+def get_course_progress(user, courses_queryset):
+    for course_object in courses_queryset:
+        sections = course_object.sections.filter(published=True)
+        sections_progress = progress(user, course_object, sections)
+
+        if sections_progress["current"] == sections_progress["max"]:
+            course_object.progress = "completed"
+        elif sections_progress["max"] > sections_progress["current"] > 0:
+            course_object.progress = "started"
+        else:
+            course_object.progress = "not started"
